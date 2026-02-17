66a60a103ec8d488436809b842ed1378824fb52b
sailor95 <bugman195@gmail.com>
2026-02-17 12:28:05 +0800

    fix: hot reload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      port: 4321,
+      hmr: {
+        clientPort: 4321,
+      },
+    },
+  },
+});
